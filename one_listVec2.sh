12d3520bb5363139077731f71e6b22b69ddcd492
Abhishek Thakur <atst@live.com>
2018-11-25 18:06:45 +0530

    added steps for AIMD

--- one_listVec2.sh
+++ one_listVec2.sh
@@ -4,20 +4,22 @@ bCmd="java -Xmx512M -cp target:lib/ECLA.jar:lib/DTNConsoleConnection.jar core.DT
 eCmd="data/reports2.txt"
 
 nonLinear="data/flow/nonLinear.txt"
-prefCC="data/prefCC.txt"
 adaptList="data/fullAdapt.txt data/2ackAdapt.txt data/1ackAdapt.txt"
-ttlList="12 24 36 48 60"
+ttlList="03 06"
+aimdList="1 2 3 4 5 6 7 8 9"
 
 for  ttl in ${ttlList} ; do
-   for  adapt in ${adaptList} ; do
-      # five runs for Full Adapt, prefCC, nonLinear, Linear, nonSVC
-      echo "${bCmd} ${nonLinear} ${adapt} ${prefCC} data/ttl/${ttl}hrs.txt ${eCmd}"
-      echo "${bCmd} data/flow/highSVC.txt ${nonLinear} ${adapt} ${prefCC} data/ttl/${ttl}hrs.txt ${eCmd}"
-      echo "${bCmd} data/flow/mediumSVC.txt ${nonLinear} ${adapt} ${prefCC} data/ttl/${ttl}hrs.txt ${eCmd}"
+   for  aimd in ${aimdList} ; do
+      for  adapt in ${adaptList} ; do
+         # Only High for RWP 50 scenario makes sense
+         # echo "${bCmd} ${nonLinear} ${adapt} data/ttl/${ttl}hrs.txt data/aImD/${aimd}.txt ${eCmd}"
+         echo "${bCmd} data/flow/highSVC.txt ${nonLinear} ${adapt} data/ttl/${ttl}hrs.txt ${eCmd}"
+         # echo "${bCmd} data/flow/mediumSVC.txt ${nonLinear} ${adapt} data/ttl/${ttl}hrs.txt ${eCmd}"
+      done
    done
-   echo "${bCmd} data/flow/nonSVC.txt ${prefCC} data/ttl/${ttl}hrs.txt ${eCmd}"
+   # echo "${bCmd} data/flow/nonSVC.txt ${prefCC} data/ttl/${ttl}hrs.txt ${eCmd}"
    echo "${bCmd} data/flow/nonSVCHigh.txt ${prefCC} data/ttl/${ttl}hrs.txt ${eCmd}"
-   echo "${bCmd} data/flow/nonSVCMedium.txt ${prefCC} data/ttl/${ttl}hrs.txt ${eCmd}"
+   # echo "${bCmd} data/flow/nonSVCMedium.txt ${prefCC} data/ttl/${ttl}hrs.txt ${eCmd}"
 done;
 
 
