c4486cd3b5b69f69fd0f500dfbd9f529b3c76ad3
vbbphc <f2016165@hyderabad.bits-pilani.ac.in>
2018-12-30 00:06:23 +0530

    Replaced layer report with summary. Add 3D plot code

--- scripts/reports.py
+++ scripts/reports.py
@@ -15,9 +15,11 @@ Run pip3 install -r requirements.txt before running this script.
 
 import os
 import sys
+import copy
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def generate_layer_report(dir_path="./rep2", dest_path="./", report_name="report.txt", run_count=None):
@@ -25,27 +27,32 @@ def generate_layer_report(dir_path="./rep2", dest_path="./", report_name="report
     Goes through all LAYER_FILE_SUFFIX file endings and generate each iteration's layer stats.
     """
     report_set = list()
-    report_data = dict()
-    report_name = report_name.replace(".txt", "_layer_.txt")
+    summary = dict()
+    rep_name = report_name.replace(".txt", "_layer_.txt")
     for reports in os.listdir(dir_path):
         if str(reports).endswith(LAYER_FILE_SUFFIX) and not str(reports).startswith('._'):
             report_set.append( str(dir_path) + '/' + str(reports))
     for report in report_set:
-        try:
-            current_iteration = int(str(report).split("_")[4])
-        except:
-            pass
-        _, current_run_data = extract_layer_data(report)
-        current_data = str(report).replace(dir_path, "").strip("/" + "\t")
-        for field in current_run_data.keys():
-            current_data = str(current_data) + str(current_run_data[field]) + "\t"
-        if report_data.__contains__(current_iteration):
-            report_data[current_iteration] += current_data + "\n"
+        # try:
+        current_iteration = str(report).strip(dir_path).strip('/').split("_")[4]
+        _, report_summary = extract_layer_data(report)
+        report_summary = [round(float(i)/5, 3) for i in report_summary]
+        sub_report_name = str(report).strip(dir_path).strip("/" + "\t").replace("_" + current_iteration, "")
+        if summary.keys().__contains__(sub_report_name):
+            for i in range(len(summary[sub_report_name])):
+                summary[sub_report_name][i] += report_summary[i]
         else:
-            report_data[current_iteration] = str(current_data)
-    for iteration in report_data.keys():
-        with open(dest_path + report_name.replace(".txt", str(iteration) + ".txt"), 'w') as f:
-            f.write(report_data[iteration])
+            summary[sub_report_name] = report_summary
+        # except Exception as e:
+        #     print(e)
+    file_summary = "Filename\taI\tmD\t0,1,2,3,4,5,6,7,8,9\n"
+    for report in summary.keys():
+        file_summary += report + "\t" + report.split("_")[6].strip("aI") + "\t" + report.split("_")[7].strip("mD") + "\t"
+        for layer in summary[report]:
+            file_summary += str(layer) + ","
+        file_summary += "\n"
+    with open(dest_path + rep_name.replace(".txt", "_summary.txt"), 'w') as f:
+        f.write(file_summary)
 
 
 def generate_report(dir_path="./rep2", dest_path="./", report_name="report.txt"):
@@ -85,9 +92,9 @@ def generate_report(dir_path="./rep2", dest_path="./", report_name="report.txt")
         report += filedata
     if dest_path.endswith("/"):
         dest_path += "/"
-    generate_layer_report(dir_path, dest_path, report_name, run_count)
     with open(dest_path+report_name, 'w') as f:
-        f.write(report)       
+        f.write(report) 
+    generate_layer_report(dir_path, dest_path, report_name, run_count)      
     plot_report_data(dest_path+report_name)
 
 
@@ -112,102 +119,175 @@ def extract_layer_data(filename=None, run_count=None):
     """
     Extracts layer data for each burst in each iteration.
     """
-    layer_report = dict()
+    layers_delivered_burst_id = dict()
     with open(filename, 'r', encoding='ISO-8859-1') as f:
         data = f.readlines()
         for line in data[1:]:
             burst_id, current_layer = line.split(" ")[1].split("_L")
-            if layer_report.keys().__contains__(burst_id):
-                if layer_report[burst_id] + 1 == int(current_layer):
-                    layer_report[burst_id] = int(current_layer)
+            if layers_delivered_burst_id.keys().__contains__(burst_id):
+                layers_delivered_burst_id[burst_id].append(int(current_layer))
+                layers_delivered_burst_id[burst_id] = sorted(layers_delivered_burst_id[burst_id])
             else:
-                layer_report[burst_id] = int(current_layer)
-    return str(filename), layer_report
-                
-def plot_report_data(report_path=None):
-    """
-    Plot's relevant additive, multiplicative data against existing fields
-    """
-    with open(report_path, 'r') as f:
-        report = f.readlines()
-        seg_data = dict()
-        for line in report[1:]:
-            data = line.split("\t")
-            ack = data[0][1]
-            if not ack == 'o':
-                addIncr = data[0].split("_")[7]
-                multDecr = data[0].split("_")[8]
-                delivery_prob, latency_avg = data[1], data[2]
-                addIncr = float(addIncr.strip("aI"))
-                multDecr = float(multDecr.strip("mD"))
-                if seg_data.keys().__contains__(ack):
-                    seg_data[ack][0].append(addIncr)
-                    seg_data[ack][1].append(multDecr)
-                    seg_data[ack][2].append(delivery_prob)
-                    seg_data[ack][3].append(latency_avg)
+                layers_delivered_burst_id[burst_id] = [int(current_layer)]
+    remove_id = []
+    for id in layers_delivered_burst_id.keys():
+        if layers_delivered_burst_id[id][0] == 0:
+            test = str()
+            test += str(layers_delivered_burst_id[id])
+            delivered_list = layers_delivered_burst_id[id]
+            i = 0
+            while i < len(delivered_list) - 1:
+                if delivered_list[i+1] == delivered_list[i] + 1:
+                    i += 1
                 else:
-                    seg_data[ack] = [[addIncr], [multDecr], [delivery_prob], [latency_avg]]
-        const_add = 0.004
-        const_mult = 0.08
-        const_add_mult_data = list()
-        const_mult_add_data = list()
-        const_add_delivery_data = list()
-        const_mult_delivery_data = list()
-        const_add_latency_data = list()
-        const_mult_latency_data = list()
-        for ackNum in seg_data.keys():
-            ack_add_delivery_data = list()
-            ack_add_latency_data = list()
-            for a in range(len(seg_data[ackNum][0])):
-                if seg_data[ackNum][0][a] ==  const_add:
-                    "Add equal"
-                    if ackNum == '1':
-                        const_add_mult_data.append(seg_data[ackNum][1][a])
-                    ack_add_delivery_data.append(round(float(seg_data[ackNum][2][a]), 6))
-                    ack_add_latency_data.append(round(float(seg_data[ackNum][3][a]), 6))
-            const_add_delivery_data.append(ack_add_delivery_data)
-            const_add_latency_data.append(ack_add_latency_data)
-            ack_mult_delivery_data = list()
-            ack_mult_latency_data = list()
-            for m in range(len(seg_data[ackNum][1])):
-                if seg_data[ackNum][1][m] ==  const_mult:
-                    if ackNum == '1':
-                        const_mult_add_data.append(seg_data[ackNum][0][m])
-                    ack_mult_delivery_data.append(round(float(seg_data[ackNum][2][m]), 6))
-                    ack_mult_latency_data.append(round(float(seg_data[ackNum][3][m]), 6))
-            const_mult_delivery_data.append(ack_mult_delivery_data)
-            const_mult_latency_data.append(ack_mult_latency_data)
-        print(const_mult_add_data)
-        print(const_add_mult_data)
-        print(const_add_latency_data)
-        print(const_add_delivery_data)
-        print(const_mult_delivery_data)
-        print(const_mult_latency_data)
-        plot_data("Additive Increase vs Latency", "Additive Increase", "Latency", const_mult_add_data, const_mult_latency_data, seg_data.keys())
-        plot_data("Additive Increase vs Delivery payloads", "Additive Increase", "Delivery payloads", const_mult_add_data, const_mult_delivery_data, seg_data.keys())
-        plot_data("Multiplicative Decrease vs Latency", "Multiplicative decrease", "Latency", const_add_mult_data, const_add_latency_data, seg_data.keys())
-        plot_data("Multiplicative Decrease vs Delivery Payload", "Multiplicative decrease", "Delivery payload", const_add_mult_data, const_add_delivery_data, seg_data.keys())
+                    break   
+            delivered_list.clear()
+            delivered_list = list(range(0, i+1))
+            print(test,delivered_list, sep='  ')
+            layers_delivered_burst_id[id] = delivered_list
+        else:
+            remove_id.append(id)
+    for i in remove_id:
+        try:
+            layers_delivered_burst_id.pop(i)
+        except:
+            pass
+    summary = [0,0,0,0,0,0,0,0,0,0]
+    for id in layers_delivered_burst_id.keys():
+        for layers in layers_delivered_burst_id[id]:
+            summary[layers] += 1
+        layers_delivered_burst_id[id] = summary
+    return str(filename), summary
+                
+def plot_report_data(report_path=None, all=True):
+    """
+    Plots relevant additive, multiplicative data against existing fields
+    """
+    if not all:
+        with open(report_path, 'r') as f:
+            report = f.readlines()
+            seg_data = dict()
+            for line in report[1:]:
+                data = line.split("\t")
+                ack = data[0][1]
+                if not ack == 'o':
+                    addIncr = data[0].split("_")[6]
+                    multDecr = data[0].split("_")[7]
+                    delivery_prob, latency_avg = data[1], data[2]
+                    addIncr = float(addIncr.strip("aI"))
+                    multDecr = float(multDecr.strip("mD"))
+                    if seg_data.keys().__contains__(ack):
+                        seg_data[ack][0].append(addIncr)
+                        seg_data[ack][1].append(multDecr)
+                        seg_data[ack][2].append(delivery_prob)
+                        seg_data[ack][3].append(latency_avg)
+                    else:
+                        seg_data[ack] = [[addIncr], [multDecr], [delivery_prob], [latency_avg]]
+            const_add = 0.004
+            const_mult = 0.08
+            const_add_mult_data = list()
+            const_mult_add_data = list()
+            const_add_delivery_data = list()
+            const_mult_delivery_data = list()
+            const_add_latency_data = list()
+            const_mult_latency_data = list()
+            for ackNum in seg_data.keys():
+                ack_add_delivery_data = list()
+                ack_add_latency_data = list()
+                for a in range(len(seg_data[ackNum][0])):
+                    if seg_data[ackNum][0][a] ==  const_add:
+                        "Add equal"
+                        if ackNum == '1':
+                            const_add_mult_data.append(seg_data[ackNum][1][a])
+                        ack_add_delivery_data.append(round(float(seg_data[ackNum][2][a]), 6))
+                        ack_add_latency_data.append(round(float(seg_data[ackNum][3][a]), 6))
+                const_add_delivery_data.append(ack_add_delivery_data)
+                const_add_latency_data.append(ack_add_latency_data)
+                ack_mult_delivery_data = list()
+                ack_mult_latency_data = list()
+                for m in range(len(seg_data[ackNum][1])):
+                    if seg_data[ackNum][1][m] ==  const_mult:
+                        if ackNum == '1':
+                            const_mult_add_data.append(seg_data[ackNum][0][m])
+                        ack_mult_delivery_data.append(round(float(seg_data[ackNum][2][m]), 6))
+                        ack_mult_latency_data.append(round(float(seg_data[ackNum][3][m]), 6))
+                const_mult_delivery_data.append(ack_mult_delivery_data)
+                const_mult_latency_data.append(ack_mult_latency_data)
+            print(const_mult_add_data)
+            print(const_add_mult_data)
+            print(const_add_latency_data)
+            print(const_add_delivery_data)
+            print(const_mult_delivery_data)
+            print(const_mult_latency_data)
+            plot_data("Additive Increase vs Latency", "Additive Increase", "Latency", const_mult_add_data, const_mult_latency_data, seg_data.keys())
+            plot_data("Additive Increase vs Delivery payloads", "Additive Increase", "Delivery payloads", const_mult_add_data, const_mult_delivery_data, seg_data.keys())
+            plot_data("Multiplicative Decrease vs Latency", "Multiplicative decrease", "Latency", const_add_mult_data, const_add_latency_data, seg_data.keys())
+            plot_data("Multiplicative Decrease vs Delivery Payload", "Multiplicative decrease", "Delivery payload", const_add_mult_data, const_add_delivery_data, seg_data.keys())
+    else:
+        with open(report_path, 'r') as f:
+            report = f.readlines()
+            addData = list()
+            multData = list()
+            delivery_prob_data = list()
+            latency_avg_data = list()
+            for line in report[1:]:
+                data = line.split("\t")
+                ack = data[0][1]
+                if not ack == 'o':
+                    addIncr = data[0].split("_")[6]
+                    multDecr = data[0].split("_")[7]
+                    delivery_prob_data.append(data[1])
+                    latency_avg_data.append(data[2])
+                    addData.append(float(addIncr.strip("aI")))
+                    multData.append(float(multDecr.strip("mD")))
+            plot_data("Delivery probability vs AddIncr vs MultDecr", "AddIncr", "MultDecr", addData, multData, None, z_data=delivery_prob_data, z_label="Delivery probability")
+            plot_data("Latency vs AddIncr vs MultDecr", "AddIncr", "MultDecr", addData, multData, None, z_data=latency_avg_data, z_label="Latency")
+        with open(report_path.replace(".txt", "_summary.txt"), 'r') as f:
+            report = f.readlines()
+            addData = list()
+            multData = list()
+            # TODO Add delivered 3D plot code
+                    
 
 
-def plot_data(graph_title=None, x_label=None, y_label=None, x_data=None, y_data=None, y_data_labels=None):
+def plot_data(graph_title=None, x_label=None, y_label=None, x_data=None, y_data=None, y_data_labels=None, all=True, z_data=None, z_label=None):
     """
     General method to plot given arguments. Only for multiple y-data
     """
-    y_data_labels = list(y_data_labels)
-    # if str(graph_title).__contains__("Latency"):
-    #     plt.gca().set_ylim([4000, 5000]) # Set's the Y limits. gca - get current axes
-    # else:
-    #     plt.gca().set_ylim([0.40, 0.55])
-    for Y in y_data:
-        x, y = zip(*sorted(zip(x_data, Y), key=lambda x: x[0]))
-        plt.plot(x, y, color=COLOURS[y_data.index(Y)], label="Ack" + str(y_data_labels[y_data.index(Y)]), marker=".")
-    plt.title(graph_title, fontsize=16, fontweight='bold')
-    plt.xlabel = x_label
-    plt.ylabel = y_label
-    plt.legend(loc="best")        
-    y_data_labels = list(y_data_labels)
-    plt.savefig(graph_title + ".png")
-    plt.clf()
+    if not all:
+        y_data_labels = list(y_data_labels)
+        # if str(graph_title).__contains__("Latency"):
+        #     plt.gca().set_ylim([0.1, 1000]) # Set's the Y limits. gca - get current axes
+        # else:
+        #     plt.gca().set_ylim([0.40, 0.55])
+        for Y in y_data:
+            x, y = zip(*sorted(zip(x_data, Y), key=lambda x: x[0]))
+            plt.plot(x, y, color=COLOURS[y_data.index(Y)], label="Ack" + str(y_data_labels[y_data.index(Y)]), marker=".")
+        #if graph_title.__contains__("Latency"):
+        plt.xscale('log', basex=10)
+        #plt.gca().set_xlim([0, 0.03]) # Set's the Y limits. gca - get current axes
+        plt.title(graph_title, fontsize=16, fontweight='bold')
+        plt.legend(loc="best")        
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        y_data_labels = list(y_data_labels)
+        plt.savefig(graph_title + ".png")
+        plt.clf()
+    else:
+        x_data = [float(i) for i in x_data]
+        y_data = [float(i) for i in y_data]
+        z_data = [float(i) for i in z_data]
+        x, y, z = zip(*sorted(zip(x_data, y_data, z_data), key=lambda x: x[1]))
+        plt.title(graph_title, fontsize=16, fontweight='bold')
+        plt.legend(loc="best")
+        fig = plt.figure()
+        ax = plt.axes(projection='3d')
+        ax.plot3D(x, y, z, COLOURS[0])
+        ax.set_xlabel = x_label
+        ax.set_ylabel = y_label
+        ax.set_zlabel = z_label
+        plt.savefig(graph_title + ".png")
+        plt.clf()
                 
 
 
